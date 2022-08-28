d737a49abeec636fca9161c933a1bc07d02acbdd
David <me@davidmiles.blog>
2022-08-27 22:27:32 -0400

    add tweet fetcher

--- webmentions.js
+++ webmentions.js
@@ -7,6 +7,12 @@ fetch(`https://webmention.io/api/links.jf2?target=${encodeURIComponent(`https://
         if (m["wm-property"] != "like-of") return;
 
         document.querySelector("#avatar").innerHTML += `<a href="${m.author.url}"><abbr title="${m.author.name}"><img src="${m.author.photo}" alt="${m.author.name}" class="avatar"></abbr></a>`
-        document.querySelector("#people").innerHTML = `${data.children.length} ${data.children.length > 1 ? "people" : "person"} liked it`
+        document.querySelector("#people").innerHTML = `${data.children.length} ${data.children.length > 1 ? "people" : "person"} liked this journal entry`
     })
   });
+fetch(`https://wmlookup.davidmiles.blog/?path=${encodeURIComponent(location.pathname)}`)  
+.then((response) => response.json())
+.then((data) => {
+if (!data.success) document.querySelector("#tweet").innerHTML = "Failed to load tweet.";
+ document.querySelector("#tweet").innerHTML = `If you want to like this post, like <a href="${data.url}">this tweet</a> :)`
+});
